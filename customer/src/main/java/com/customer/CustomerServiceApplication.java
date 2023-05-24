1ac8d46e4735523a602d7895d7693db00919ed47
Prasad <prasadsp2001@gmail.com>
2023-05-24 19:03:05 +0530

    Cors Error solved

--- customer/src/main/java/com/customer/CustomerServiceApplication.java
+++ customer/src/main/java/com/customer/CustomerServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,6 +15,15 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer(){
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry){
+//				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+//			}
+//		};
+//	}
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate(){
--- medicine/src/main/java/com/medicine/MedicineServiceApplication.java
+++ medicine/src/main/java/com/medicine/MedicineServiceApplication.java
@@ -2,7 +2,10 @@ package com.medicine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MedicineServiceApplication {
@@ -10,5 +13,14 @@ public class MedicineServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedicineServiceApplication.class, args);
 	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer(){
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry){
+//				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+//			}
+//		};
+//	}
 
 }
